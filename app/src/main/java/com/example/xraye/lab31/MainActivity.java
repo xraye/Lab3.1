04ac410226acfc8a2026689ab16cf89ae804e4bb
joshuacrook <jcrook@ucsd.edu>
2016-04-18 16:26:44 -0700

    Initial commit

---
+++ app/src/main/java/com/example/xraye/lab31/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.xraye.lab31;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected int sum(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button add = (Button) findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+
+            final EditText num1 = (EditText) findViewById(R.id.num1);
+            final EditText num2 = (EditText) findViewById(R.id.num2);
+            final TextView answer = (TextView) findViewById(R.id.ans);
+
+            @Override
+            public void onClick(View v) {
+                int n1 = Integer.parseInt(num1.getText().toString());
+                int n2 = Integer.parseInt(num2.getText().toString());
+                int sumI = sum(n1, n2);
+                answer.setText(String.valueOf(sumI));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
